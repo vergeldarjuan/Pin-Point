fe125cda0a34f3623713b165441c1077b77a7b95
vergeldarjuan <vergelsdarjuan@gmail.com>
2025-06-29 01:53:13 +0800

    organized the files in the src directory and added new images for the logo and house icons. converting it into a mobile app interface.

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import javax.swing.SwingUtilities;
+import javax.swing.UIManager;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            try {
+                UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+            } catch (Exception ignored) {
+            }
+            new NavigationUI().setVisible(true);
+        });
+    }
+}
